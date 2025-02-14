9bba88855b40843055095bc499ae55447bc48c5f
Vivek <viveksinghcharak004@gmail.com>
2025-02-14 16:01:31 +0530

    only checking if ec2 works with the domain name

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = 5500;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  try {
+    res.status(200).json({ msg: "hello world!" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+app.post("/", (req, res) => {
+  try {
+    console.log(req.body);
+    const { name } = req.body;
+    res.status(200).json({ msg: `hello ${name}` });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port : ${PORT}`);
 });
-
