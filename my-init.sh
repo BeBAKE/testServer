c8d24a0ab632be3d9f2e797d36feef6e56907dff
Vivek <viveksinghcharak004@gmail.com>
2025-04-03 15:31:20 +0530

    making the certbot command fully non interactive

--- my-init.sh
+++ my-init.sh
@@ -4,27 +4,29 @@
 #   echo "docker is not installed, installing it"
 #   sudo apt update
 
-#   sudo apt install curl apt-transport-https ca-certificates software-properties-common
+  sudo apt install curl apt-transport-https ca-certificates software-properties-common
 
-#   sudo apt install docker.io -y
+  sudo apt install docker.io -y
 
-#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-#   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#   sudo apt update
+  sudo apt update
 
-#   sudo apt install docker-ce -y
+  sudo apt install docker-ce -y
 
-#   sudo systemctl start docker
+  sudo systemctl start docker
 
-#   sudo apt install git
+  sudo apt install git
 
-#   git clone https://github.com/BeBAKE/testServer.git
+  git clone https://github.com/BeBAKE/testServer.git
 
-#   cd testServer
+  cd testServer
 
-#   sudo docker compose up -d
+  chmod +x my-init.sh
+
+  # sudo docker compose up -d
 
 #   sleep 10
 
@@ -34,11 +36,13 @@
 #   exit 1
 # fi
 
-
+echo "starting docker compose up"
 sudo docker compose up -d
 
 sleep 5
-
+echo "copying files"
 cat ./nginx/afterSSLNginx.txt > ./nginx/nginx.conf
 
+sleep 1
+echo "reloading nginx"
 sudo docker compose exec nginx nginx -s reload
